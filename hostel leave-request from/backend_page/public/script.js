f00776c99ec51b0851f632eac225412ea81d5b7b
vimal-vs <60288021+vimal-vs@users.noreply.github.com>
2022-10-06 22:27:24 +0530

    Update script.js

--- hostel leave-request from/backend_page/public/script.js
+++ hostel leave-request from/backend_page/public/script.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAzWc74eM8G7yOXpfwzB-pnIJZEq2i_TEg",
-  authDomain: "hostelleaverequestform-a9fd4.firebaseapp.com",
-  databaseURL: "https://hostelleaverequestform-a9fd4-default-rtdb.firebaseio.com",
-  projectId: "hostelleaverequestform-a9fd4",
-  storageBucket: "hostelleaverequestform-a9fd4.appspot.com",
-  messagingSenderId: "905877805852",
-  appId: "1:905877805852:web:3db48c86e49749bece38d9",
+  //firebase config
 };
 
 firebase.initializeApp(firebaseConfig);
